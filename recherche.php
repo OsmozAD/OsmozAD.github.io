f1c9854ecdacc6becc607084c1887337ad33dfa6
OsmozAD <strike.elite59@gmail.com>
2023-08-31 13:20:15 +0200

    Update recherche.php

--- recherche.php
+++ recherche.php
@@ -1,6 +1,6 @@
 <?php
 // Connexion à la base de données (à personnaliser avec vos informations)
-$mysqli = new mysqli('localhost', 'root', 'Achille59+', 'Base');
+$mysqli = new mysqli('localhost', 'root', 'root', 'Base');
 
 // Vérification de la connexion
 if ($mysqli->connect_error) {
