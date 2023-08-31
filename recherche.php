11ca50f5384895f6f60c698739e5f8f5f41fa376
OsmozAD <strike.elite59@gmail.com>
2023-08-31 13:56:54 +0200

    Update recherche.php

--- recherche.php
+++ recherche.php
@@ -17,17 +17,33 @@ if (isset($_POST['query'])) {
     // Exécution de la requête
     $result = $mysqli->query($sql);
 }
-
-// Affichage des résultats
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        echo "<p>Nom : " . $row['name'] . "</p>";
-        echo "<p>Description : " . $row['desc'] . "</p>";
-    }
-} else {
-    echo "Aucun résultat trouvé.";
-}
-
-// Fermeture de la connexion à la base de données
-$mysqli->close();
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Recherche de users</title>
+</head>
+<body>
+    <h1>Recherche de données</h1>
+    <form action="recherche.php" method="POST">
+        <input type="text" name="query" placeholder="Recherche...">
+        <button type="submit">Rechercher</button>
+    </form>
+    <section class="afficher_recherche">
+	<?php
+		if ($result->num_rows > 0) {
+		    while ($row = $result->fetch_assoc()) {
+        		echo "<p>Nom : " . $row['name'] . "</p>";
+       			echo "<p>Description : " . $row['desc'] . "</p>";
+    		    }
+		} else {
+    		    echo "Aucun résultat trouvé.";
+		}
+
+		$mysqli->close();
+	?>
+    </section>
+</body>
+</html>
